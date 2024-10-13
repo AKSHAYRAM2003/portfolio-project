c630c5e634a3f453c7f9ace417745e37e53d56b4
Akshay  H R <ramakshay100@gmail.com>
2024-10-13 17:52:52 +0530

    grain effect added and project setup completed

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image';
+import { clsx } from 'clsx';
+import{cn} from '@/lib/utils';
+
+export default function Home() {
+  return(
+    <div>
+      <h1 className="font-oswald">hello my freinds iam akshay</h1>
+      <h1 className="">hello my freinds iam akshay</h1>
+    </div>
+  );
+}
